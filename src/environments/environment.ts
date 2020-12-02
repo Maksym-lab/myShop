0f1642df8d66b88ac80ea5c6af82581ace3690df
Maksym-lab <maxim.sinchilo@devit-team.com>
2020-12-02 04:11:05 +0000

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAQNmTP93t7jh9g6doFYrxAOnnly1frcs8",
+    authDomain: "little-shop-angular.firebaseapp.com",
+    databaseURL: "https:
+    projectId: "little-shop-angular",
+    storageBucket: "little-shop-angular.appspot.com",
+    messagingSenderId: "869448801378",
+    appId: "1:869448801378:web:359e65987f5de36e"
+  }
 };
