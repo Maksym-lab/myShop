5b893e31dd04ed7055250335ca9e272d7e52857a
Maksym-lab <maxim.sinchilo@devit-team.com>
2020-12-21 01:07:19 +0000

    Search in admin products

--- src/app/controllers/administrator-controller/components/products-list/products-list.component.ts
+++ src/app/controllers/administrator-controller/components/products-list/products-list.component.ts
@@ -1,19 +1,22 @@
 import { ProductService } from '../../../../services/product/product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product/product';
 @Component({
   selector: 'products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnDestroy {
   products$ : Observable<Product[]>;
   productsList : Product[];
+  filtered: any[];
+  subscriptions: Subscription;
   constructor(private productService: ProductService,
     private router: Router) { 
      this.products$ = this.productService.getProducts();
+     this.subscriptions = this.products$.subscribe(prod => this.filtered = this.productsList = prod);
   }
   ngOnInit() {
   }
@@ -22,5 +25,13 @@ export class ProductsListComponent implements OnInit {
   }
   onDelete(id){
     this.productService.deleteProduct(id);
+  }
+  filter(query: string){
+    this.filtered = (query) ? 
+    this.productsList.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+    this.productsList;
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
